ccbbef0e1ed91de65649da2ab3bd62a84f647992
ka-amina <aminakara400@gmail.com>
2025-03-14 14:27:04 +0000

    ajouter la fonctionalite de la modification des hard skills

--- resources/views/Admin/hardSkills/edithardskill.blade.php
+++ resources/views/Admin/hardSkills/edithardskill.blade.php
@@ -4,10 +4,12 @@
     <div id="categoryModal" class="fixed flex inset-0 justify-center items-center bg-gray-500 bg-opacity-50 z-50 ">
             <div class="bg-white rounded-lg p-6 max-w-sm w-full">
                 <h3 class="text-xl font-semibold text-gray-800 mb-4">Edit Hardskill: name</h3>
-                <form action="#">
+                <form action="{{route('hardskills.update',$hardSkill->id)}}" method="POST">
+                    @csrf
+                    @method('PUT')
                     <div class="mb-4">
                         <label for="category_name" class="block text-sm text-gray-700">skill Name</label>
-                        <input type="text" name="category_name" id="category_name" class="mt-1 block w-full border p-2 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" >
+                        <input type="text" name="name" id="category_name" class="mt-1 block w-full border p-2 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" value="{{ $hardSkill->name }}" >
                     </div>
                     <div class="flex justify-end space-x-2">
                         <button type="button" id="closeModalBtn" class="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400"><a href="{{route('hardskills')}}">Cancel</a></button>
